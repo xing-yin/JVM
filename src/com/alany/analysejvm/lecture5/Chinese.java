7ba97713792153c61fa570a1e2ce7b562dce1465
yinxing <yinxing@dxy.cn>
2019-10-23 13:00:34 +0800

    [add]新增拆解 JVM 04/05 代码

---
+++ src/com/alany/analysejvm/lecture5/Chinese.java
@@ -0,0 +1,13 @@
+package com.alany.analysejvm.lecture5;
+
+/**
+ * @author yinxing
+ * @date 2019/10/22
+ */
+
+public class Chinese extends Passenger{
+
+    @Override
+    void goAbroad() {
+    }
+}
